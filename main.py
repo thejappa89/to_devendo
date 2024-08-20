379f1c0ab3bf28e5b1e817381e2129a24ce9217d
Henrique Lima (Jappa) <jappadev@gmail.com>
2024-08-19 23:28:04 -0300

    Inserindo informações no arquivo

---
+++ main.py
@@ -0,0 +1,30 @@
+# CONTROLE DE EMPRESTIMO PESSOAL
+
+base_emprestimo = 500
+base_juros = 15
+
+# INFORMAÇÕES INICIAIS
+print('''
+DADOS GERAIS DO EMPRÉSTIMO PESSOAL
+      Valor: R$ {} reais
+      Juros: {}%
+      '''.format(base_emprestimo, base_juros))
+
+# CALCULANDO
+saldo_anterior = base_emprestimo
+vlr_pago = 100
+vlr_juros = saldo_anterior * 0.15
+vlr_capital = vlr_pago - vlr_juros
+saldo_atual = saldo_anterior - vlr_capital
+
+# INFORMAÇÕES GERAIS
+print('''
+DETALHAMENTO MENSAL DE PAGAMENTOS
+      Saldo anterior: {}
+
+      Valor pago: R$ {}
+        {} juros
+        {} capital
+      
+      Saldo atualizado: {}
+      '''.format(saldo_anterior, vlr_pago, vlr_juros, vlr_capital, saldo_atual))
