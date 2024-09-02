a92d3580b0c99c573bed52dfa7da7c8df22b4867
Henrique Lima (Jappa) <jappadev@gmail.com>
2024-09-01 21:46:53 -0300

    melhorias fixadas

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import jappalib.visual
+from time import sleep
 
 
 base_emprestimo = 500
 base_juros = 15
-
-# INFORMAÇÕES INICIAIS
-print('''
-DADOS GERAIS DO EMPRÉSTIMO PESSOAL
-      Valor: R$ {} reais
-      Juros: {}%
-      '''.format(base_emprestimo, base_juros))
 
 # CALCULANDO
 saldo_anterior = base_emprestimo
@@ -18,33 +12,30 @@ vlr_juros = saldo_anterior * 0.15
 vlr_capital = vlr_pago - vlr_juros
 saldo_atual = saldo_anterior - vlr_capital
 
-# INFORMAÇÕES GERAIS
-print('''
-DETALHAMENTO MENSAL DE PAGAMENTOS
-      Saldo anterior: {}
-
-      Valor pago: R$ {}
-        {} juros
-        {} capital
-      Saldo atualizado: {}
-      '''.format(saldo_anterior,
-                 vlr_pago,
-                 vlr_juros,
-                 vlr_capital,
-                 saldo_atual
-                 ))
-
 # MENU PRINCIPAL
 while True:
-    resp = jappalib.visual.menu(['Adicionar pagamento',
-                                 'Consultar extrato',
-                                 'Sair...'])
+	resp = jappalib.visual.menu(['Dados do emprestimo', 'Adicionar pagamento',
+								 'Extrato detalhado',
+								 'Sair...'])
 
-    if resp == 1:
-        print('Adicionar pagamento')
-    elif resp == 2:
-        print('Consultar pagamento')
-    elif resp == 3:
-        print('Saindo...')
-    else:
-        print('\033[mERRO: Opção inválida! Tente novamente.\033[m')
+	if resp == 1:
+		jappalib.visual.titulo('DADOS DO EMPRÉSTIMO')
+		print('Valor: R$ {} reais\nJuros: {}%\n'.format(base_emprestimo, base_juros))
+		sleep(2)
+	
+	elif resp == 2:
+		jappalib.visual.titulo('ADICIONAR PAGAMENTO')
+		sleep(2)
+	
+	elif resp == 3:
+		jappalib.visual.titulo('EXTRATO DETALHADO')
+		sleep(2)
+	
+	elif resp == 4:
+		print('Encerrando o sistema, aguarde!')
+		sleep(2)
+		print('Finalizado com sucesso!')
+		break
+
+	else:
+		print('ERRO: Opção inválida! Tente novamente.')
